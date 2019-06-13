7c9156c882b516b6114a8359a5047459b3bc2d0b
davetorrente <whaepekk@gmail.com>
2019-06-13 11:05:43 +0800

    remove app.php and fix indentions

--- config/functions.php
+++ config/functions.php
@@ -1,5 +1,4 @@
 <?php
- $errors = array();
 date_default_timezone_set('Asia/Manila');
 /* Function for redirect
 /*--------------------------------------------------------------*/
@@ -8,12 +7,12 @@ function redirect($url)
     if (headers_sent() === false) {
         header('Location: ' . $url, true);
     }
-
     exit();
 }
 /*--------------------------------------------------------------*/
 /* Function for  Readable Make date time
 /*--------------------------------------------------------------*/
-function makeDate(){
-  return strftime("%Y-%m-%d %H:%M:%S", time());
+function makeDate()
+{
+    return strftime("%Y-%m-%d %H:%M:%S", time());
 }
