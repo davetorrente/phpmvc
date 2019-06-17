daad028755d94eaace25566b1839c41317d58211
davetorrente <whaepekk@gmail.com>
2019-06-17 18:23:06 +0800

    online db

--- config/env.php
+++ config/env.php
@@ -1,12 +1,12 @@
 <?php
-    define('DB_HOST', 'localhost'); // Set database host
-    define('DB_USER', 'root'); // Set database user
-    define('DB_PASS', ''); // Set database password
-    define('DB_NAME', 'php_mvc');  // Set database name
-	define("DS", DIRECTORY_SEPARATOR);
+    define('DB_HOST', 'remotemysql.com'); // Set database host
+    define('DB_USER', 'J95e7iOAiv'); // Set database user
+    define('DB_PASS', '0bMS4HEfpg'); // Set database password
+    define('DB_NAME', 'J95e7iOAiv');  // Set database name
+    define("DS", DIRECTORY_SEPARATOR);
 
-	// -----------------------------------------------------------------------
-	// DEFINE ROOT PATHS
-	// -----------------------------------------------------------------------
-	defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
-	define("LIB_PATH_INC", SITE_ROOT.DS);
+    // -----------------------------------------------------------------------
+    // DEFINE ROOT PATHS
+    // -----------------------------------------------------------------------
+    defined('SITE_ROOT')? null: define('SITE_ROOT', realpath(dirname(__FILE__)));
+    define("LIB_PATH_INC", SITE_ROOT.DS);
